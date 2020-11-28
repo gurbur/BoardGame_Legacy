abfdee410c80b3684fe35b92d2abb07ac7121a18
gurbur331 <gurbur331@ajou.ac.kr>
2020-11-28 23:38:33 +0900

    add showingNum and getter, for YachtDice

--- TermProject/src/dice/Dice.java
+++ TermProject/src/dice/Dice.java
@@ -1,8 +1,14 @@
 package dice;
 
 public class Dice {
+	private int showingNum = 1;
 	public int roll() {
 		int output= (int)(Math.random()*6+1);
+		showingNum = output;
 		return output;
+	}
+	
+	public int getShowingNum() {
+		return showingNum;
 	}
 }
